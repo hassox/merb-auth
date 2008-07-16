925b8710a4fe5144eadb53bd016b315378a29797
Daniel Neighman <has.sox@gmail.com>
2008-07-16 14:27:16 +1000

    Removes skip_before filter.  This isn't needed in the slice version since it doesn't inherit from application

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,5 +1,3 @@
 class MerbAuth::Sessions < MerbAuth::Application
-  
-  skip_before :login_required
-  
+
 end
--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,5 +1,3 @@
 class MerbAuth::Users < MerbAuth::Application
   
-  skip_before :login_required
-  
 end
